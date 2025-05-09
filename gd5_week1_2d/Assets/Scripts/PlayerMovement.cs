b6dc3c77164f445f0e36195789d5e2ddc031130c
akshatsinha1 <akshat.sinha@techeducators.co.uk>
2025-05-09 13:55:42 +0100

    Initial Commit
    - Created a 2D URP Project
    - Added a background
    - Added a simple 2D player controller
    - Ability to die

---
+++ gd5_week1_2d/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform translate for movement
+        //transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime,0,0);
+
+        //movement using the physics engine/ rigidbody
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * Input.GetAxis("Horizontal") * speed);
+
+        if(transform.position.y < -14)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+
+    }
+}
